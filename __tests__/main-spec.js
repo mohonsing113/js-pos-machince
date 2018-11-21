933df6d6c62b69c3ae9891b5e1d957842fee10e8
System Administrator <root@Mos-MacBook-Air.local>
2018-11-21 21:21:37 +0800

    add weight checking, translate to eng

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,107 +1,113 @@
 const printReceipt = require('../main');
 
 function loadAllItems() {
-    return [
-      {
-        barcode: 'ITEM000000',
-        name: '可口可乐',
-        unit: '瓶',
-        price: 3.00
-      },
-      {
-        barcode: 'ITEM000001',
-        name: '雪碧',
-        unit: '瓶',
-        price: 3.00
-      },
-      {
-        barcode: 'ITEM000002',
-        name: '苹果',
-        unit: '斤',
-        price: 5.50
-      },
-      {
-        barcode: 'ITEM000003',
-        name: '荔枝',
-        unit: '斤',
-        price: 15.00
-      },
-      {
-        barcode: 'ITEM000004',
-        name: '电池',
-        unit: '个',
-        price: 2.00
-      },
-      {
-        barcode: 'ITEM000005',
-        name: '方便面',
-        unit: '袋',
-        price: 4.50
-      }
-    ];
-  }
+  return [
+    {
+      barcode: 'ITEM000000',
+      name: 'Coca-Cola',
+      unit: 'bottle',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000001',
+      name: 'Sprite',
+      unit: 'bottle',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000002',
+      name: 'Apple',
+      unit: 'kg',
+      price: 5.50
+    },
+    {
+      barcode: 'ITEM000003',
+      name: 'Litchi',
+      unit: 'kg',
+      price: 15.00
+    },
+    {
+      barcode: 'ITEM000004',
+      name: 'Battery',
+      unit: 'box',
+      price: 2.00
+    },
+    {
+      barcode: 'ITEM000005',
+      name: 'Noodles',
+      unit: 'bag',
+      price: 4.50
+    }
+  ];
+}
 
-  function loadPromotions() {
-    return [
-      {
-        type: 'BUY_TWO_GET_ONE_FREE',
-        barcodes: [
-          'ITEM000000',
-          'ITEM000001',
-          'ITEM000005'
-        ]
-      }
-    ];
-  }
+function loadPromotions() {
+  return [
+    {
+      type: 'BUY_TWO_GET_ONE_FREE',
+      barcodes: [
+        'ITEM000000',
+        'ITEM000001',
+        'ITEM000005'
+      ]
+    }
+  ];
+}
   
 
   
-  it ('should print only 1 item recepit', () => {
+  it ('should print only 1 item receipt', () => {
     const input = [
         'ITEM000001'
       ]
     expect(printReceipt(input, loadAllItems(), loadPromotions())).toBe(
-        "***<没钱赚商店>购物清单***\n"+
-        "名称：雪碧, 数量：1瓶, 单价：3.00(元), 小计：3.00(元)\n"+
+        "***<store earning no money>Receipt***\n"+
+        "Name：Sprite, Quantity：1bottle, Unit price：3.00(yuan), Subtotal：3.00(yuan)\n"+
         "----------------------\n"+
-        "Saving：0.00(元)\n"+
-        "总计：3.00(元)\n"+
+        "Saving：0.00(yuan)\n"+
+        "Total：3.00(yuan)\n"+
         "**********************"
     );
   });
 
-  it ('should print only 1 item recepit', () => {
+  it ('should print only 1 item receipt with saving', () => {
     const input = [
         'ITEM000001',
         'ITEM000001',
         'ITEM000001'
       ]
     expect(printReceipt(input, loadAllItems(), loadPromotions())).toBe(
-        "***<没钱赚商店>购物清单***\n"+
-        "名称：雪碧, 数量：3瓶, 单价：3.00(元), 小计：9.00(元)\n"+
-        "----------------------\n"+
-        "Saving：3.00(元)\n"+
-        "总计：6.00(元)\n"+
-        "**********************"
+        "***<store earning no money>Receipt***\n"+
+         "Name：Sprite, Quantity：3bottle, Unit price：3.00(yuan), Subtotal：9.00(yuan)\n"+
+         "----------------------\n"+
+         "Saving：3.00(yuan)\n"+
+         "Total：6.00(yuan)\n"+
+         "**********************"
     );
   });
 
-// });
-// it ('should print receipt', () => {
-//     const input = [
-//         'ITEM000001',
-//         'ITEM000001',
-//         'ITEM000001',
-//         'ITEM000001',
-//         'ITEM000001',
-//         //'ITEM000003-2',
-//         'ITEM000005',
-//         'ITEM000005',
-//         'ITEM000005'
-//       ]
-//     expect(printReceipt(input, loadAllItems(), loadPromotions())).toBe(null
 
-//     );
-// });
+ it ('should print receipt with weight checking and saving', () => {
+     const input = [
+         'ITEM000001',
+         'ITEM000001',
+         'ITEM000001',
+         'ITEM000001',
+         'ITEM000001',
+         'ITEM000003-2',
+         'ITEM000005',
+         'ITEM000005',
+         'ITEM000005'
+       ]
+     expect(printReceipt(input, loadAllItems(), loadPromotions())).toBe(
+        "***<store earning no money>Receipt***\n"+
+         "Name：Sprite, Quantity：5bottle, Unit price：3.00(yuan), Subtotal：15.00(yuan)\n"+
+         "Name：Litchi, Quantity：2kg, Unit price：15.00(yuan), Subtotal：30.00(yuan)\n"+
+         "Name：Noodles, Quantity：3bag, Unit price：4.50(yuan), Subtotal：13.50(yuan)\n"+
+         "----------------------\n"+
+         "Saving：7.50(yuan)\n"+
+         "Total：51.00(yuan)\n"+
+         "**********************"
 
-
+     );
+ });
--- main.js
+++ main.js
@@ -1,36 +1,44 @@
 function printReceipt (barcodes, database, promotions) {
     let itemList=[]
     barcodes.forEach(barcode => {
-        addItemToReceipt(itemList, find(barcode, database))
+        let splitBarcode = barcode.split('-')
+        let bar = splitBarcode[0]
+        let weight = splitBarcode[1]
+        if(!isNaN(weight)){
+            weight = parseInt(weight)
+        }else{
+            weight = 1
+        }
+        addItemToReceipt(itemList, find(bar, database), weight)
     });
 
     let text = ""
-    let total =0;
+    let total = 0;
 
-    text+='***<没钱赚商店>购物清单***\n'
+    text+='***<store earning no money>Receipt***\n'
     itemList.forEach( item =>{
-        text+="名称："+item.name+", "
-        text+="数量："+item.count+item.unit+", "
-        text+="单价："+item.price.toFixed(2)+"(元), "
+        text+="Name："+item.name+", "
+        text+="Quantity："+item.count+item.unit+", "
+        text+="Unit price："+item.price.toFixed(2)+"(yuan), "
         let smallTotal = item.price*item.count
-        text+="小计："+smallTotal.toFixed(2)+"(元)\n"
+        text+="Subtotal："+smallTotal.toFixed(2)+"(yuan)\n"
         total+=smallTotal
     })
     
     let saving =applyPromotion(itemList, promotions)
     total-=saving
 
-    text+= '----------------------\n' +'Saving：'+saving.toFixed(2)+'(元)\n'
-    text+= '总计：'+total.toFixed(2)+'(元)\n' + '**********************'
+    text+= '----------------------\n' +'Saving：'+saving.toFixed(2)+'(yuan)\n'
+    text+= 'Total：'+total.toFixed(2)+'(yuan)\n' + '**********************'
     return text
 }
 
-function addItemToReceipt(itemList, item){
+function addItemToReceipt(itemList, item, weight){
     let itemInReceipt = find(item.barcode, itemList)
     if(typeof itemInReceipt != 'undefined'){
-        itemInReceipt.count++
+        itemInReceipt.count+=weight
     }else{
-        itemList.push({barcode:item.barcode, name:item.name, count:1, price:item.price, unit:item.unit})
+        itemList.push({barcode:item.barcode, name:item.name, count:weight, price:item.price, unit:item.unit})
     }
 }
 
@@ -42,15 +50,15 @@ function applyPromotion(itemList, promotions){
     let saving = 0
     promotions.forEach(promo =>{
         if(promo.type == 'BUY_TWO_GET_ONE_FREE'){
-
             itemList
                 .filter(item => typeof find(item.id, promo.barcodes) != undefined)
-                .filter(item => item.count%3==0)
+                .filter(item => item.count/3>=1)
                 .map(item =>{
                     saving += item.price* parseInt(item.count/3)
                 });
-                
+
         }
+        //space for future promos
     });
     return saving
 }
