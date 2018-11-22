bc7072441da2bc9c8cd2a8b4de112576b61fded0
Mo Hon Sing <h.s.mo@oocl.com>
2018-11-22 11:37:45 +0800

    refactor applyPromotion

--- main.js
+++ main.js
@@ -1,35 +1,29 @@
 function printReceipt (barcodes, database, promotions) {
     let itemList=[]
+
     barcodes.forEach(barcode => {
         let splitBarcode = barcode.split('-')
         let bar = splitBarcode[0]
-        let weight = splitBarcode[1]
-        if(!isNaN(weight)){
-            weight = parseInt(weight)
-        }else{
-            weight = 1
-        }
+        let weight = isNaN(splitBarcode[1])? 1 : parseInt(splitBarcode[1])
         addItemToReceipt(itemList, find(bar, database), weight)
     });
 
-    let text = ""
     let total = 0;
-
-    text+='***<store earning no money>Receipt***\n'
+    let text ='***<store earning no money>Receipt***\n'
     itemList.forEach( item =>{
-        text+="Name："+item.name+", "
-        text+="Quantity："+item.count+item.unit+", "
-        text+="Unit price："+item.price.toFixed(2)+"(yuan), "
+        text+="Name："+item.name+", "+"Quantity："+item.count+item.unit+", "+"Unit price："+item.price.toFixed(2)+"(yuan), "
         let smallTotal = item.price*item.count
         text+="Subtotal："+smallTotal.toFixed(2)+"(yuan)\n"
         total+=smallTotal
     })
     
-    let saving =applyPromotion(itemList, promotions)
+    let saving = applyPromotion(itemList, promotions)
     total-=saving
 
-    text+= '----------------------\n' +'Saving：'+saving.toFixed(2)+'(yuan)\n'
-    text+= 'Total：'+total.toFixed(2)+'(yuan)\n' + '**********************'
+    text+= '----------------------\n' 
+        + 'Saving：'+saving.toFixed(2)+'(yuan)\n'
+        + 'Total：'+total.toFixed(2)+'(yuan)\n' 
+        + '**********************'
     return text
 }
 
@@ -42,24 +36,19 @@ function addItemToReceipt(itemList, item, weight){
     }
 }
 
-function find(barcode, list){
-    return list.find(a => a.barcode==barcode)
-}
+function find(barcode, list){return list.find(a => a.barcode==barcode)}
+
+function sum(a, b) {return a + b}
 
 function applyPromotion(itemList, promotions){
-    let saving = 0
-    promotions.forEach(promo =>{
+    return promotions.map(promo =>{
         if(promo.type == 'BUY_TWO_GET_ONE_FREE'){
-            itemList
-                .filter(item => typeof find(item.id, promo.barcodes) != undefined)
+            return itemList.filter(item => typeof find(item.id, promo.barcodes) != undefined)
                 .filter(item => item.count/3>=1)
-                .map(item =>{
-                    saving += item.price* parseInt(item.count/3)
-                });
-
+                .map(item => item.price*parseInt(item.count/3) )
+                .reduce(sum, 0);
         }
         //space for future promos
-    });
-    return saving
+    }).reduce(sum, 0);
 }
 module.exports = printReceipt;
